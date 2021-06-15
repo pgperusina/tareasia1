945b4ea33fc485698b5e52319901a2f846992492
Pablo G. Perusina <pablo.garcia@conexiogroup.com>
2021-06-15 14:17:17 -0600

    refactor

--- aspiradora/aspiradora.js
+++ aspiradora/aspiradora.js
@@ -1,10 +1,5 @@
 
 function aspirar(location, state){
-    statusMap.forEach(function(value, key) {
-      if (value == states) visitedStates.set(key, true);
-    })
-    console.log(visitedStates);
-
     if (state=="DIRTY") return "CLEAN";
     else if (location=="A") return "RIGHT";
     else if (location=="B") return "LEFT";
@@ -30,17 +25,5 @@ ensuciar = () => {
   if (Math.floor(Math.random() * 2) == 0) states[2] = "DIRTY";  
 }
 
-// 0 = dirty - 1 = clean
 var states = ["A","DIRTY","DIRTY"];
-var visitedStates = new Map();
-var statusMap = new Map();
-var todosVisitados = false;
-statusMap.set[0, "A","DIRTY","DIRTY"];
-statusMap.set[1, "A","DIRTY","CLEAN"];
-statusMap.set[2, "A","CLEAN","DIRTY"];
-statusMap.set[3, "A","CLEAN","CLEAN"];
-statusMap.set[4, "B","DIRTY","DIRTY"];
-statusMap.set[5, "B","DIRTY","CLEAN"];
-statusMap.set[6, "B","CLEAN","DIRTY"];
-statusMap.set[7, "B","CLEAN","CLEAN"];
 verificar(states);
